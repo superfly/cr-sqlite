53d72fec903dcedfaf77cc3909e81a90ff4ffd5f
Grzegorz Uriasz <grzegorz@fly.io>
2026-08-14 20:00:23 +0200

    Fix utrig trigger name in teardown

--- core/rs/core/src/teardown.rs
+++ core/rs/core/src/teardown.rs
@@ -36,26 +36,6 @@ pub fn remove_crr_triggers_if_exist(
         table = escaped_table
     ))?;
 
-    // Collect pk col names first, then drop triggers after statement is finalized
-    // to avoid LOCKED errors from schema modifications while a stmt is active.
-    let pk_cols: Vec<String> = {
-        let stmt = db.prepare_v2("SELECT name FROM pragma_table_info(?) WHERE pk > 0")?;
-        stmt.bind_text(1, table, sqlite::Destructor::STATIC)?;
-        let mut cols = Vec::new();
-        while stmt.step()? == ResultCode::ROW {
-            cols.push(String::from(stmt.column_text(0)?));
-        }
-        cols
-    };
-
-    for col_name in &pk_cols {
-        db.exec_safe(&format!(
-            "DROP TRIGGER IF EXISTS \"{tbl_name}_{col_name}__crsql_utrig\"",
-            tbl_name = crate::util::escape_ident(table),
-            col_name = crate::util::escape_ident(col_name),
-        ))?;
-    }
-
     db.exec_safe(&format!(
         "DROP TRIGGER IF EXISTS \"{table}__crsql_dtrig\"",
         table = escaped_table
